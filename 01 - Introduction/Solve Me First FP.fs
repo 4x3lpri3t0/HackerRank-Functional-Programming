2c524f597c035428f37219022dbce6cf272d890f
Axel Prieto <prietoaxel@gmail.com>
2016-08-15 22:57:02 -0300

    Daily programming practice

---
+++ 01 - Introduction/Solve Me First FP.fs
@@ -0,0 +1,12 @@
+﻿namespace _01___Introduction
+
+open System
+
+module SolveMeFirstFP =
+    type T = {A:int; B:int} with
+        member this.Sum =
+            this.A + this.B
+
+    // constructor
+    let create a b = 
+        {A=a; B=b}
---
+++ Program.fs
@@ -0,0 +1,12 @@
+﻿open System
+open _01___Introduction
+
+[<EntryPoint>]
+let main argv = 
+    let a = Console.ReadLine() |> int
+    let b = Console.ReadLine() |> int
+    let solveMeFirstFP = SolveMeFirstFP.create a b
+    let sum = solveMeFirstFP.Sum
+    printfn "%d" (sum)
+    
+    0 // return an integer exit code
